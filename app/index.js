4153e6ebe422e9949a8b9c2204ca7392d45bf7ce
kermage <genealyson.torcende@gmail.com>
2015-11-16 03:21:07 +0800

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,44 @@
+var yeoman = require('yeoman-generator');
+var async = require( 'async' );
+
+var Generator = yeoman.generators.Base.extend({
+	promptUser: function() {
+		var done = this.async();
+		var prompts = [
+			{
+				name: 'themeName',
+				message: 'Theme Name:',
+			},
+			{
+				name: 'themeURI',
+				message: 'Theme URI:',
+			},
+			{
+				name: 'author',
+				message: 'Author:',
+			},
+			{
+				name: 'authorURI',
+				message: 'Author URI:',
+			},
+			{
+				name: 'description',
+				message: 'Description:',
+			},
+			{
+				name: 'functionPrefix',
+				message: 'Function Prefix:',
+			}
+		];
+		this.prompt(prompts, function (props) {
+			this.opts = props;
+			done();
+		}.bind(this));
+	},
+	theme: function() {
+		this.template( '_style.css', 'style.css' );
+		this.template( '_functions.php', 'functions.php' );
+	}
+});
+
+module.exports = Generator;
---
+++ app/templates/_functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * <%= opts.themeName %> functions and definitions
+ *
+ * @package <%= opts.themeName %>
+ * @since 0.1.0
+ */
+
+/* ==================================================
+Global constants
+================================================== */
+define( 'THEME_NAME',		wp_get_theme()->get( 'Name' ) );
+define( 'THEME_VERSION',	wp_get_theme()->get( 'Version' ) );
+define( 'THEME_URI',		wp_get_theme()->get( 'ThemeURI' ) );
+define( 'THEME_AUTHOR',		wp_get_theme()->get( 'Author' ) );
+define( 'AUTHOR_URI',		wp_get_theme()->get( 'AuthorURI' ) );
+define( 'PARENT_THEME',		wp_get_theme()->get( 'Template' ) );
+define( 'THEME_URL',		get_stylesheet_directory_uri() . '/' );
+define( 'THEME_PATH',		get_stylesheet_directory() . '/' );
+define( 'THEME_INC',		THEME_PATH . 'inc/' );
+define( 'THEME_DEBUG',		true );
+
