6b6448c907f7c17d22abd030236cd077304be8b2
kermage <genealyson.torcende@gmail.com>
2015-11-23 05:47:00 +0800

    register sidebars to offer widgets in footer area

--- app/templates/theme/inc/_widgets.php
+++ app/templates/theme/inc/_widgets.php
@@ -10,11 +10,38 @@
 if( ! function_exists( '<%= opts.functionPrefix %>_widgets_init' ) ) {
 	function <%= opts.functionPrefix %>_widgets_init() {
 		register_sidebar( array(
-			'id'			=> 'sidebar-1',
+			'id'			=> 'sidebar',
 			'name'			=> __( 'Sidebar Area', '<%= opts.functionPrefix %>' ),
 			'description'	=> __( 'Add widgets here to appear in sidebar.', '<%= opts.functionPrefix %>' ),
 			'before_title'	=> '<h3 class="widget-title">',
 			'after_title'	=> '</h3>',
+			'before_widget'	=> '<section class="widget %2$s">',
+			'after_widget'	=> '</section>'
+		) );
+		register_sidebar( array(
+			'id'			=> 'footer-1',
+			'name'			=> __( 'Footer Area1', '<%= opts.functionPrefix %>' ),
+			'description'	=> __( 'Add widgets here to appear in footer.', '<%= opts.functionPrefix %>' ),
+			'before_title'	=> '<h4 class="widget-title">',
+			'after_title'	=> '</h4>',
+			'before_widget'	=> '<section class="widget %2$s">',
+			'after_widget'	=> '</section>'
+		) );
+		register_sidebar( array(
+			'id'			=> 'footer-2',
+			'name'			=> __( 'Footer Area2', '<%= opts.functionPrefix %>' ),
+			'description'	=> __( 'Add widgets here to appear in footer.', '<%= opts.functionPrefix %>' ),
+			'before_title'	=> '<h4 class="widget-title">',
+			'after_title'	=> '</h4>',
+			'before_widget'	=> '<section class="widget %2$s">',
+			'after_widget'	=> '</section>'
+		) );
+		register_sidebar( array(
+			'id'			=> 'footer-3',
+			'name'			=> __( 'Footer Area3', '<%= opts.functionPrefix %>' ),
+			'description'	=> __( 'Add widgets here to appear in footer.', '<%= opts.functionPrefix %>' ),
+			'before_title'	=> '<h4 class="widget-title">',
+			'after_title'	=> '</h4>',
 			'before_widget'	=> '<section class="widget %2$s">',
 			'after_widget'	=> '</section>'
 		) );
