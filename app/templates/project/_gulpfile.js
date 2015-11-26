d99d5e4f64bacc6019e31ffab9a6dc3bcdfc02ee
kermage <genealyson.torcende@gmail.com>
2015-11-26 18:32:41 +0800

    added project task: plumber

--- app/templates/project/_gulpfile.js
+++ app/templates/project/_gulpfile.js
@@ -6,11 +6,14 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	cssnano = require('gulp-cssnano'),
 	browserSync = require('browser-sync'),
-	notify = require('gulp-notify');
+	notify = require('gulp-notify'),
+	plumber = require('gulp-plumber');
 
 gulp.task('concat', function(){
 	gulp.src('assets/js/*.js')
+		.pipe(plumber())
 		.pipe(concat('<%= opts.projectSlug %>.js'))
+		.pipe(plumber.stop())
 		.pipe(gulp.dest('js'))
 		.pipe(browserSync.stream())
 		.pipe(notify({message: 'Concat task complete', onLast: true }));
@@ -18,8 +21,10 @@ gulp.task('concat', function(){
 
 gulp.task('uglify', function(){
 	gulp.src(['js/*.js','!js/*.min.js'])
+		.pipe(plumber())
 		.pipe(uglify())
 		.pipe(rename({suffix: '.min'}))
+		.pipe(plumber.stop())
 		.pipe(gulp.dest('js'))
 		.pipe(browserSync.stream())
 		.pipe(notify({message: 'Uglify task complete', onLast: true }));
@@ -27,11 +32,13 @@ gulp.task('uglify', function(){
 
 gulp.task('imagemin', function(){
 	gulp.src('assets/images/*.{gif,jpg,png}')
+		.pipe(plumber())
 		.pipe(imagemin({
 			optimizationLevel: 7,
 			progressive: true,
 			interlaced: true
 		}))
+		.pipe(plumber.stop())
 		.pipe(gulp.dest('images'))
 		.pipe(browserSync.stream())
 		.pipe(notify({message: 'Imagemin task complete', onLast: true }));
@@ -39,9 +46,11 @@ gulp.task('imagemin', function(){
 
 gulp.task('sass', function(){
 	gulp.src('assets/sass/*.{scss,sass}')
+		.pipe(plumber())
 		.pipe(sass({
 			outputStyle: 'expanded'
 		}))
+		.pipe(plumber.stop())
 		.pipe(gulp.dest('css'))
 		.pipe(browserSync.stream())
 		.pipe(notify({message: 'Sass task complete', onLast: true }));
@@ -49,10 +58,12 @@ gulp.task('sass', function(){
 
 gulp.task('cssnano', function(){
 	gulp.src(['css/*.css','!css/*.min.css'])
+		.pipe(plumber())
 		.pipe(cssnano({
 			discardComments:{removeAllButFirst:true}
 		}))
 		.pipe(rename({suffix: '.min'}))
+		.pipe(plumber.stop())
 		.pipe(gulp.dest('css'))
 		.pipe(browserSync.stream())
 		.pipe(notify({message: 'Cssnano task complete', onLast: true }));
