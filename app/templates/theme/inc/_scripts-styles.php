83b1681a44a54df4e6774ceb7697eb894acede4c
kermage <genealyson.torcende@gmail.com>
2016-09-05 21:10:15 +0800

    use updated jquery, add fontawesome, and load site style and script last

--- app/templates/theme/inc/_scripts-styles.php
+++ app/templates/theme/inc/_scripts-styles.php
@@ -11,17 +11,19 @@ if( ! function_exists( '<%= opts.functionPrefix %>_scripts_styles' ) ) {
     function <%= opts.functionPrefix %>_scripts_styles() {
         $suffix = ( WP_DEBUG || SCRIPT_DEBUG || THEME_DEBUG ) ? '' : '.min';
         
-        // Site scripts and styles
-        wp_enqueue_style( '<%= opts.functionPrefix %>-style', THEME_URL . 'css/<%= opts.projectSlug %>' . $suffix . '.css', array(), THEME_VERSION, false );
-        wp_enqueue_script( '<%= opts.functionPrefix %>-script', THEME_URL . 'js/<%= opts.projectSlug %>' . $suffix . '.js', array(), THEME_VERSION, true );
-        
         // Deregister the jquery version bundled with WordPress
         wp_deregister_script( 'jquery' );
         // CDN hosted jQuery placed in the header, as some plugins require that jQuery is loaded in the header
-        wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery' . $suffix . '.js', array(), '2.1.4', false );
+        wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery' . $suffix . '.js', array(), '2.2.4', false );
         
         // Google Fonts
         wp_enqueue_style( '<%= opts.functionPrefix %>-fonts', '//fonts.googleapis.com/css?family=Lato:400,700,900|Open+Sans:400,600,800' );
+        // Font Awesome
+        wp_enqueue_style( '<%= opts.functionPrefix %>-fontawesome', '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome' . $suffix . '.css', array(), '4.6.3', false );
+        
+        // Site scripts and styles
+        wp_enqueue_style( '<%= opts.functionPrefix %>-style', THEME_URL . 'css/<%= opts.projectSlug %>' . $suffix . '.css', array(), THEME_VERSION, false );
+        wp_enqueue_script( '<%= opts.functionPrefix %>-script', THEME_URL . 'js/<%= opts.projectSlug %>' . $suffix . '.js', array(), THEME_VERSION, true );
     }
     add_action( 'wp_enqueue_scripts', '<%= opts.functionPrefix %>_scripts_styles', 20 );
 }
