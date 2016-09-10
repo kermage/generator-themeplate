963148671d8b53f95d2d0530e4bf2b95d9868f0f
kermage <genealyson.torcende@gmail.com>
2016-09-10 16:18:28 +0800

    custom settings setup and test entries

--- app/templates/theme/inc/settings/_entries.php
+++ app/templates/theme/inc/settings/_entries.php
@@ -11,6 +11,36 @@ if( ! function_exists( '<%= opts.functionPrefix %>_settings' ) ) {
     function <%= opts.functionPrefix %>_settings() {
         register_setting( '<%= opts.functionPrefix %>', '<%= opts.functionPrefix %>' );
         $prefix = '<%= opts.functionPrefix %>_';
+        
+        <%= opts.functionPrefix %>_add_settings( array(
+            'id'        => $prefix . 'general',
+            'title'     => __( 'General', '<%= opts.functionPrefix %>' ),
+            'callback'  => '',
+            'fields'    => array(
+                $prefix . 'description' => array(
+                    'name'      => __( 'Description', '<%= opts.functionPrefix %>' ),
+                    'type'      => 'text'
+                ),
+                $prefix . 'copyright' => array(
+                    'name'      => __( 'Copyright', '<%= opts.functionPrefix %>' ),
+                    'type'      => 'textarea'
+                ),
+                $prefix . 'template' => array(
+                    'name'      => __( 'Default Template', '<%= opts.functionPrefix %>' ),
+                    'type'      => 'select',
+                    'options'   => array( 'Header; Footer', 'No Header; Footer', 'Header; No Footer', 'No Header; No Footer' )
+                ),
+                $prefix . 'layout' => array(
+                    'name'      => __( 'Default Layout', '<%= opts.functionPrefix %>' ),
+                    'type'      => 'radio',
+                    'options'   => array( 'Content Left; Sidebar Right', 'Content Right; Sidebar Left', 'Fullwidth Page' )
+                ),
+                $prefix . 'disable' => array(
+                    'name'      => __( 'Disable Titles', '<%= opts.functionPrefix %>' ),
+                    'type'      => 'checkbox'
+                )
+            )
+        ) );
     }
     add_action( 'admin_init', '<%= opts.functionPrefix %>_settings' );
 }
--- app/templates/theme/inc/settings/_setup.php
+++ app/templates/theme/inc/settings/_setup.php
@@ -41,3 +41,72 @@ if( ! function_exists( '<%= opts.functionPrefix %>_settings_page' ) ) {
         <?php
     }
 }
+
+if( ! function_exists( '<%= opts.functionPrefix %>_add_settings' ) ) {
+    function <%= opts.functionPrefix %>_add_settings( $param ) {
+        if ( ! is_array( $param ) )
+            return false;
+        
+        add_settings_section(
+            $param['id'],
+            $param['title'],
+            $param['callback'],
+            '<%= opts.functionPrefix %>'
+        );
+        
+        foreach ( $param['fields'] as $id => $field ) {
+            add_settings_field(
+                $id,
+                $field['name'],
+                '<%= opts.functionPrefix %>_create_settings',
+                '<%= opts.functionPrefix %>',
+                $param['id'],
+                array(
+                    'label_for' => $id,
+                    'type'      => $field['type'],
+                    'options'   => $field['options']
+                )
+            );
+        }
+    }
+}
+
+if( ! function_exists( '<%= opts.functionPrefix %>_create_settings' ) ) {
+    function <%= opts.functionPrefix %>_create_settings( $param ) {
+        if ( ! is_array( $param ) )
+            return false;
+        
+        $id = $param['label_for'];
+        $setting = get_option( '<%= opts.functionPrefix %>' );
+        $setting = $setting[$id];
+        
+        switch ( $param['type'] ) {
+            default:
+            case 'text':
+                echo '<input type="text" name="<%= opts.functionPrefix %>[' . $id . ']" id="' . $id . '" value="' . $setting . '" />';
+                break;
+                
+            case 'textarea':
+                echo '<textarea name="<%= opts.functionPrefix %>[' . $id . ']" id="' . $id . '" rows="4">' . $setting . '</textarea>';
+                break;
+                
+            case 'select' :
+                echo '<select name="<%= opts.functionPrefix %>[' . $id . ']" id="' . $id . '">';
+                foreach( $param['options'] as $value => $option ) {
+                    echo '<option value="' . ( $value + 1 ) . '"' . selected( $setting, ( $value + 1 ), false ) . '>' . $option . '</option>';
+                }
+                echo '</select>';
+                break;
+                
+            case 'radio' :
+                foreach( $param['options'] as $value => $option ) {
+                    echo '<label><input type="radio" name="<%= opts.functionPrefix %>[' . $id . ']" value="' . ( $value +  1 ) . '"' . checked( $setting, ( $value +  1 ), false ) . ' /> ' . $option . '</label>';
+                }
+                break;
+                
+            case 'checkbox' :
+                echo '<input type="checkbox" name="<%= opts.functionPrefix %>[' . $id . ']" id="' . $id . '" value="1" ' . checked( $setting, 1, false ) . ' />';
+                break;
+        }
+    }
+}
