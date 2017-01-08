a80b19c5195208ba1c7b33a24b0aa3beaaeaf8ff
kermage <genealyson.torcende@gmail.com>
2017-01-09 02:46:41 +0800

    rework walker; extend the main walker class and better filter of default classes

--- app/templates/theme/inc/_nav-walker.php
+++ app/templates/theme/inc/_nav-walker.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
- * Custom Nav Walker
+ * Custom Clean Nav Walker
  *
  * @package <%= opts.themeName %>
  * @since 0.1.0
  */
 
 if( ! class_exists( '<%= opts.classPrefix %>_nav_walker' ) ) {
-	class <%= opts.classPrefix %>_nav_walker extends Walker_Nav_Menu {
+	class <%= opts.classPrefix %>_nav_walker extends Walker {
+		public $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
+
 		public function start_lvl( &$output, $depth = 0, $args = array() ) {
 			$output .= '<ul class="sub-menu">';
 		}
@@ -19,10 +21,9 @@ if( ! class_exists( '<%= opts.classPrefix %>_nav_walker' ) ) {
 
 		public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-			if( in_array( 'menu-item-has-children', (array) $classes ) ) $classes[] = 'has-sub';
-			if( in_array( 'current-menu-item', (array) $classes ) ) $classes[] = 'active';
-			$classes = preg_replace( '/(current(-menu-|[-_]page[-_])(item|parent|ancestor))/', '', $classes );
-			$classes = preg_replace( '/^((menu|page)[-_\w+]+)+/', '', $classes );
+			$classes = preg_replace( '/current[-_](menu|page)[-_](item|parent|ancestor)|(menu|page)[-_\w+]+/', '', $classes );
+			if ( $args->walker->has_children ) $classes[] = 'has-sub';
+			if ( $item->current ) $classes[] = 'active';
 			$classes = join( ' ', array_filter( $classes ) );
 			$output .= '<li' . ( ( $classes ) ? ' class="' . esc_attr( $classes ) . '"' : '' ) . '>';
 
