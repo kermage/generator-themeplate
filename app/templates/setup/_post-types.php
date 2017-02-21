bf2ec056fc42b48210e727cc0f1d641c0356ccaa
kermage <genealyson.torcende@gmail.com>
2017-02-21 08:26:29 +0800

    cleanup; format whitespace

--- app/templates/setup/_post-types.php
+++ app/templates/setup/_post-types.php
@@ -12,63 +12,63 @@ if( ! function_exists( '<%= opts.functionPrefix %>_post_types' ) ) {
 		$prefix = '<%= opts.functionPrefix %>_';
 
 		themeplate_add_post_type( array(
-			'name'          => $prefix . 'portfolio',
-			'plural'        => __( 'Portfolio', '<%= opts.functionPrefix %>' ),
-			'singular'      => __( 'Portfolio', '<%= opts.functionPrefix %>' ),
-			'description'   => __( 'Portfolio', '<%= opts.functionPrefix %>' ),
+			'name'        => $prefix . 'portfolio',
+			'plural'      => __( 'Portfolio', '<%= opts.functionPrefix %>' ),
+			'singular'    => __( 'Portfolio', '<%= opts.functionPrefix %>' ),
+			'description' => __( 'Portfolio', '<%= opts.functionPrefix %>' ),
 			'args' => array(
-				'public'                => true,
-				// 'exclude_from_search'   => false,
-				// 'publicly_queryable'    => true,
-				// 'show_ui'               => true,
-				// 'show_in_nav_menus'     => true,
-				// 'show_in_menu'          => true,
-				// 'show_in_admin_bar'     => true,
-				'menu_position'         => 5,
-				'menu_icon'             => 'dashicons-media-document',
-				'capability_type'       => 'post',
-				'hierarchical'          => false,
-				'supports'              => array( 'title', 'editor', 'thumbnail' ),
-				'has_archive'           => true,
-				'rewrite'               => array( 'slug' => 'portfolio', 'with_front' => false )
+				'public'              => true,
+				// 'exclude_from_search' => false,
+				// 'publicly_queryable'  => true,
+				// 'show_ui'             => true,
+				// 'show_in_nav_menus'   => true,
+				// 'show_in_menu'        => true,
+				// 'show_in_admin_bar'   => true,
+				'menu_position'       => 5,
+				'menu_icon'           => 'dashicons-media-document',
+				'capability_type'     => 'post',
+				'hierarchical'        => false,
+				'supports'            => array( 'title', 'editor', 'thumbnail' ),
+				'has_archive'         => true,
+				'rewrite'             => array( 'slug' => 'portfolio', 'with_front' => false )
 			)
 		) );
 
 		themeplate_add_taxonomy( array(
-			'name'          => $prefix . 'portfolio-cat',
-			'plural'        => __( 'Categories', '<%= opts.functionPrefix %>' ),
-			'singular'      => __( 'Category', '<%= opts.functionPrefix %>' ),
-			'description'   => __( 'Portfolio Category', '<%= opts.functionPrefix %>' ),
-			'type'          => $prefix . 'portfolio',
+			'name'        => $prefix . 'portfolio-cat',
+			'plural'      => __( 'Categories', '<%= opts.functionPrefix %>' ),
+			'singular'    => __( 'Category', '<%= opts.functionPrefix %>' ),
+			'description' => __( 'Portfolio Category', '<%= opts.functionPrefix %>' ),
+			'type'        => $prefix . 'portfolio',
 			'args' => array(
-				'public'                => true,
-				// 'show_ui'               => true,
-				// 'show_in_menu'          => true,
-				// 'show_in_nav_menus'     => true,
-				// 'show_tagcloud'         => true,
-				// 'show_in_quick_edit'    => true,
-				'show_admin_column'     => true,
-				'hierarchical'          => true,
-				'rewrite'               => array( 'slug' => 'portfolio-cat' )
+				'public'             => true,
+				// 'show_ui'            => true,
+				// 'show_in_menu'       => true,
+				// 'show_in_nav_menus'  => true,
+				// 'show_tagcloud'      => true,
+				// 'show_in_quick_edit' => true,
+				'show_admin_column'  => true,
+				'hierarchical'       => true,
+				'rewrite'            => array( 'slug' => 'portfolio-cat' )
 			)
 		) );
 
 		themeplate_add_taxonomy( array(
-			'name'          => $prefix . 'portfolio-tag',
-			'plural'        => __( 'Tags', '<%= opts.functionPrefix %>' ),
-			'singular'      => __( 'Tag', '<%= opts.functionPrefix %>' ),
-			'description'   => __( 'Portfolio Tag', '<%= opts.functionPrefix %>' ),
-			'type'          => $prefix . 'portfolio',
+			'name'        => $prefix . 'portfolio-tag',
+			'plural'      => __( 'Tags', '<%= opts.functionPrefix %>' ),
+			'singular'    => __( 'Tag', '<%= opts.functionPrefix %>' ),
+			'description' => __( 'Portfolio Tag', '<%= opts.functionPrefix %>' ),
+			'type'        => $prefix . 'portfolio',
 			'args' => array(
-				'public'                => true,
-				// 'show_ui'               => true,
-				// 'show_in_menu'          => true,
-				// 'show_in_nav_menus'     => true,
-				// 'show_tagcloud'         => true,
-				// 'show_in_quick_edit'    => true,
-				'show_admin_column'     => true,
-				'hierarchical'          => false,
-				'rewrite'               => array( 'slug' => 'portfolio-tag' )
+				'public'             => true,
+				// 'show_ui'            => true,
+				// 'show_in_menu'       => true,
+				// 'show_in_nav_menus'  => true,
+				// 'show_tagcloud'      => true,
+				// 'show_in_quick_edit' => true,
+				'show_admin_column'  => true,
+				'hierarchical'       => false,
+				'rewrite'            => array( 'slug' => 'portfolio-tag' )
 			)
 		) );
 	}
