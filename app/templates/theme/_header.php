52f8285f4493ddcf58b3217f1459029d89a5b45e
kermage <genealyson.torcende@gmail.com>
2017-03-31 06:43:57 +0800

    prevent the page from scaling to fit the viewport

--- app/templates/theme/_header.php
+++ app/templates/theme/_header.php
@@ -12,7 +12,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<?php wp_head(); ?>
 	</head>
 
