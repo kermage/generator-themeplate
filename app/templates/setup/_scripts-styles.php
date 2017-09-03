838fff010bea255f54da28c5e6a37c2f95c507b8
kermage <genealyson.torcende@gmail.com>
2017-09-03 17:22:13 +0800

    easily add more localized variables; using filter

--- app/templates/setup/_scripts-styles.php
+++ app/templates/setup/_scripts-styles.php
@@ -26,7 +26,11 @@ if ( ! function_exists( '<%= opts.functionPrefix %>_scripts_styles' ) ) {
 		// Site scripts and styles
 		wp_enqueue_style( '<%= opts.functionPrefix %>-style', THEME_URL . 'css/<%= opts.projectSlug %>' . $suffix . '.css', array(), THEME_VERSION, false );
 		wp_enqueue_script( '<%= opts.functionPrefix %>-script', THEME_URL . 'js/<%= opts.projectSlug %>' . $suffix . '.js', array(), THEME_VERSION, true );
-		wp_localize_script( '<%= opts.functionPrefix %>-script', '<%= opts.functionPrefix %>_options', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+
+		$<%= opts.functionPrefix %>_options = array(
+			'ajaxurl' => admin_url( 'admin-ajax.php' )
+		);
+		wp_localize_script( '<%= opts.functionPrefix %>-script', '<%= opts.functionPrefix %>_options', apply_filters( '<%= opts.functionPrefix %>_localize_script', $<%= opts.functionPrefix %>_options ) );
 	}
 	add_action( 'wp_enqueue_scripts', '<%= opts.functionPrefix %>_scripts_styles', 20 );
 }
