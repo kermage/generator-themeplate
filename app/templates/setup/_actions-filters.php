fcfb04d4de7bcd2028c3c2945cbc970a3666c102
kermage <genealyson.torcende@gmail.com>
2018-01-23 00:07:42 +0800

    no longer required since 4.9 ~ 4.8.1

--- app/templates/setup/_actions-filters.php
+++ app/templates/setup/_actions-filters.php
@@ -9,9 +9,6 @@
 
 add_action( 'after_switch_theme', 'flush_rewrite_rules' );
 
-// Enable shortcodes in widgets
-add_filter( 'widget_text', 'do_shortcode' );
-
 // Remove JPEG compression.
 if ( ! function_exists( '<%= opts.functionPrefix %>_jpeg_quality' ) ) {
 	function <%= opts.functionPrefix %>_jpeg_quality() {
