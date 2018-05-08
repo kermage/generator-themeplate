147648b4e4c3446217f5e6f48d4b99fa8071e25b
kermage <genealyson.torcende@gmail.com>
2018-05-09 03:14:44 +0800

    add meta tag for IE emulation

--- app/templates/theme/_header.php
+++ app/templates/theme/_header.php
@@ -13,6 +13,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 		<?php wp_head(); ?>
 	</head>
