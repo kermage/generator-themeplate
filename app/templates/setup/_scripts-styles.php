352536d7f10058c0902fd2a31e40c70f37b3cd88
kermage <genealyson.torcende@gmail.com>
2018-09-13 08:59:25 +0800

    fontawesome v5.3.1

--- app/templates/setup/_scripts-styles.php
+++ app/templates/setup/_scripts-styles.php
@@ -24,7 +24,7 @@ if ( ! function_exists( '<%= opts.functionPrefix %>_scripts_styles' ) ) {
 		// Google Fonts
 		wp_enqueue_style( '<%= opts.functionPrefix %>-fonts', 'https://fonts.googleapis.com/css?family=Lato:400,700,900|Open+Sans:400,600,800' );<% if ( opts.fontawesome ) { %>
 		// Font Awesome
-		wp_enqueue_script( '<%= opts.functionPrefix %>-fontawesome', 'https://use.fontawesome.com/releases/v5.0.13/js/all.js', array(), '5.0.13', false );
+		wp_enqueue_script( '<%= opts.functionPrefix %>-fontawesome', 'https://use.fontawesome.com/releases/v5.3.1/js/all.js', array(), '5.3.1', false );
 		wp_add_inline_script( '<%= opts.functionPrefix %>-fontawesome', 'FontAwesomeConfig = { searchPseudoElements: true };' );<% } %><% if ( opts.bootstrap ) { %>
 		// Bootstrap
 		wp_enqueue_script( '<%= opts.functionPrefix %>-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap' . $suffix . '.js', array(), '4.1.3', true );<% } %>
