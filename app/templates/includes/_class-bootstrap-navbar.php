38eba9ac5d6f0fe43468bdc57d4dd735d12ff308
kermage <genealyson.torcende@gmail.com>
2018-12-15 00:05:35 +0800

    dont fail if ThemePlate is not yet available

--- app/templates/includes/_class-bootstrap-navbar.php
+++ app/templates/includes/_class-bootstrap-navbar.php
@@ -7,25 +7,29 @@
  * @since 0.1.0
  */
 
-if ( ! class_exists( 'Bootstrap_NavBar' ) && class_exists( 'ThemePlate_NavWalker' ) ) {
-	class Bootstrap_NavBar extends ThemePlate_NavWalker {
-		public $class = array(
-			'sub-menu' => 'dropdown-menu',
-			'has-sub'  => 'dropdown',
-			'active'   => 'active',
-		);
+if ( ! class_exists( 'Bootstrap_NavBar' ) ) {
+	if ( class_exists( 'ThemePlate_NavWalker' ) ) {
+		class Bootstrap_NavBar extends ThemePlate_NavWalker {
+			public $class = array(
+				'sub-menu' => 'dropdown-menu',
+				'has-sub'  => 'dropdown',
+				'active'   => 'active',
+			);
 
-		public function attributes( $item, $args ) {
-			$atts = array();
+			public function attributes( $item, $args ) {
+				$atts = array();
 
-			if ( $args->walker->has_children ) {
-				$atts['href']          = '#';
-				$atts['data-toggle']   = 'dropdown';
-				$atts['class']         = 'dropdown-toggle';
-				$atts['aria-haspopup'] = 'true';
+				if ( $args->walker->has_children ) {
+					$atts['href']          = '#';
+					$atts['data-toggle']   = 'dropdown';
+					$atts['class']         = 'dropdown-toggle';
+					$atts['aria-haspopup'] = 'true';
+				}
+
+				return $atts;
 			}
-
-			return $atts;
 		}
+	} else {
+		class Bootstrap_NavBar extends Walker_Nav_Menu {}
 	}
 }
