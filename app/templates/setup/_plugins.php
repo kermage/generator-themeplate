111fb9417a3d9ddc150da9d1458b5bc466b41440
kermage <genealyson.torcende@gmail.com>
2018-12-16 22:51:02 +0800

    use releases download link; latest v3.0.0

--- app/templates/setup/_plugins.php
+++ app/templates/setup/_plugins.php
@@ -17,7 +17,7 @@ if ( ! function_exists( '<%= opts.functionPrefix %>_plugins' ) ) {
 				'name'             => 'ThemePlate',
 				'slug'             => 'themeplate',
 				'required'         => true,
-				'source'           => 'https://github.com/kermage/ThemePlate/archive/master.zip',
+				'source'           => 'https://github.com/kermage/ThemePlate/releases/download/v3.0.0/themeplate.zip',
 				'force_activation' => true,
 			),
 			array(
