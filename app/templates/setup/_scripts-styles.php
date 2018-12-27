e21630b527c42937c64911c0318778c1c3ee156c
kermage <genealyson.torcende@gmail.com>
2018-12-28 00:58:46 +0800

    update to bootstrap v4.2.1

--- app/templates/setup/_scripts-styles.php
+++ app/templates/setup/_scripts-styles.php
@@ -27,7 +27,7 @@ if ( ! function_exists( '<%= opts.functionPrefix %>_scripts_styles' ) ) {
 		wp_enqueue_script( '<%= opts.functionPrefix %>-fontawesome', 'https://use.fontawesome.com/releases/v5.6.1/js/all.js', array(), '5.6.1', false );
 		wp_add_inline_script( '<%= opts.functionPrefix %>-fontawesome', 'FontAwesomeConfig = { searchPseudoElements: true };' );<% } %><% if ( opts.bootstrap ) { %>
 		// Bootstrap
-		wp_enqueue_script( '<%= opts.functionPrefix %>-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap' . $suffix . '.js', array(), '4.1.3', true );<% } %>
+		wp_enqueue_script( '<%= opts.functionPrefix %>-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap' . $suffix . '.js', array(), '4.2.1', true );<% } %>
 
 		// Site scripts and styles
 		wp_enqueue_style( '<%= opts.functionPrefix %>-style', THEME_URL . 'assets/css/<%= opts.projectSlug %>' . $suffix . '.css', array(), THEME_VERSION );
