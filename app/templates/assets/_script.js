e7c1eb25f1bcbc25e7c45aa8d8a5410f1cc1fa2c
kermage <genealyson.torcende@gmail.com>
2019-07-31 08:50:19 +0800

    format to wanted coding style

--- app/templates/assets/_script.js
+++ app/templates/assets/_script.js
@@ -1,8 +1,6 @@
-(function( $ ) {
-
+( function( $ ) {
 	$( document ).ready( function() {
 		// eslint-disable-next-line no-console
 		console.log( 'Everything is ready. ThemePlate!' );
-	});
-
-}( jQuery ));
+	} );
+}( jQuery ) );
