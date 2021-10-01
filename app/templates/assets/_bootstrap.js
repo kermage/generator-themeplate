315dab7ee03c442ac4e489c0a9a6e5e74fa5ad84
kermage <genealyson.torcende@gmail.com>
2021-10-01 21:57:10 +0800

    update to bootstrap v5

--- app/templates/assets/_bootstrap.js
+++ app/templates/assets/_bootstrap.js
@@ -1,33 +1,33 @@
+/**
+ * --------------------------------------------------------------------------
+ * Bootstrap (v5.1.1): index.umd.js
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+ * --------------------------------------------------------------------------
+ */
+
 import Alert from 'bootstrap/js/src/alert';
 import Button from 'bootstrap/js/src/button';
 import Carousel from 'bootstrap/js/src/carousel';
 import Collapse from 'bootstrap/js/src/collapse';
 import Dropdown from 'bootstrap/js/src/dropdown';
 import Modal from 'bootstrap/js/src/modal';
+import Offcanvas from 'bootstrap/js/src/offcanvas';
 import Popover from 'bootstrap/js/src/popover';
-import Scrollspy from 'bootstrap/js/src/scrollspy';
+import ScrollSpy from 'bootstrap/js/src/scrollspy';
 import Tab from 'bootstrap/js/src/tab';
 import Toast from 'bootstrap/js/src/toast';
 import Tooltip from 'bootstrap/js/src/tooltip';
-import Util from 'bootstrap/js/src/util';
 
-/**
- * --------------------------------------------------------------------------
- * Bootstrap (v4.5.3): index.js
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
- * --------------------------------------------------------------------------
- */
-
-export {
-	Util,
+export default {
 	Alert,
 	Button,
 	Carousel,
 	Collapse,
 	Dropdown,
 	Modal,
+	Offcanvas,
 	Popover,
-	Scrollspy,
+	ScrollSpy,
 	Tab,
 	Toast,
 	Tooltip,
