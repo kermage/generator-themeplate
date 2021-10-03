89fa6e282bb025e582b529e06edb449bf8846605
kermage <genealyson.torcende@gmail.com>
2021-10-03 21:26:19 +0800

    use the new property and method names

--- app/templates/theme/includes/class-clean-navbar.php
+++ app/templates/theme/includes/class-clean-navbar.php
@@ -10,17 +10,15 @@
 if ( ! class_exists( 'Clean_Navbar' ) ) {
 	if ( class_exists( 'ThemePlate\NavWalker' ) ) {
 		class Clean_Navbar extends ThemePlate\NavWalker {
-			public $class = array(
+			public $classes = array(
 				'sub-menu' => 'dropdown-menu',
 				'has-sub'  => 'dropdown',
 				'active'   => 'active',
 				'item'     => 'nav-item',
 			);
 
-			public function attributes( $item, $args ) {
-				$atts = array(
-					'class' => 'nav-link',
-				);
+			public function link_attributes( $atts, $item, $args, $depth ) {
+				$atts['class'] = 'nav-link';
 
 				if ( $args->walker->has_children ) {
 					$atts['class']        .= ' dropdown-toggle';
