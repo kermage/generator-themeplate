7d83bbeab677d104c63ffc920c372c348922981f
kermage <genealyson.torcende@gmail.com>
2022-05-28 11:49:02 +0800

    more useful banner heading

--- app/templates/theme/template-parts/_global-banner.php
+++ app/templates/theme/template-parts/_global-banner.php
@@ -7,10 +7,24 @@
  * @since 0.1.0
  */
 
+if ( is_single() || is_404() ) {
+	return;
+}
+
+if ( is_archive() ) {
+	$heading = get_the_archive_title();
+} else {
+	$heading = single_post_title( '', false );
+}
+
+if ( empty( $heading ) ) {
+	return;
+}
+
 ?>
 
 <div class="banner">
 	<div class="container">
-		<h1><?php single_post_title(); ?></h1>
+		<h1><?php echo esc_html( $heading ); ?></h1>
 	</div>
 </div><!-- .banner -->
