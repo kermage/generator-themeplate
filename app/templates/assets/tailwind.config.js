6b3415d78a16fc6f72d2f292fe67ecee2867e342
kermage <genealyson.torcende@gmail.com>
2022-06-19 09:30:34 +0800

    update to tailwind v3

--- app/templates/assets/tailwind.config.js
+++ app/templates/assets/tailwind.config.js
@@ -1,17 +1,10 @@
 module.exports = {
-	purge: {
-		content: [
-			'./*.php',
-			'./template-parts/**/*.php',
-			'./page-templates/**/*.php',
-			'./src/js/**/*.js',
-		],
-		options: {
-			fontFace: true,
-			keyframes: true,
-		},
-	},
-	darkMode: false, // or 'media' or 'class'
+	content: [
+		"./*.php",
+		"./template-parts/**/*.php",
+		"./page-templates/**/*.php",
+		"./src/js/**/*.js",
+	],
 	theme: {
 		extend: {},
 	},
