bbd2f4ca54b48f47a7af45f5368b605b5c84581c
kermage <genealyson.torcende@gmail.com>
2023-02-09 21:21:46 +0800

    global jQuery and ignore others not in camelCase

--- app/templates/assets/_script.js
+++ app/templates/assets/_script.js
@@ -1,8 +1,7 @@
-import $ from 'jquery';
 <%_ if ( 'bootstrap' === opts.framework ) { _%>
 import './_bootstrap';
 <% } %>
-$(document).ready(function () {
+jQuery(document).ready(function () {
 	// eslint-disable-next-line no-console
 	console.log('Everything is ready. ThemePlate!');
 });
