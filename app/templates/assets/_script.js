6962bb5fd9bb597d40ebdb012100bfc7caf23db0
kermage <genealyson.torcende@gmail.com>
2024-03-02 17:46:15 +0800

    simplify project files

--- app/templates/assets/_script.js
+++ app/templates/assets/_script.js
@@ -1,8 +1,9 @@
+// @ts-check
 <%_ if ( 'bootstrap' === opts.framework ) { _%>
 import './_bootstrap';
 
 <%_ } _%>
-jQuery(document).ready(function () {
+jQuery(function () {
 	// eslint-disable-next-line no-console
 	console.log('Everything is ready. ThemePlate!');
 });
